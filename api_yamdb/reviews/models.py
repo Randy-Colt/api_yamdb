344439aac0dd8acfeca7e3af9ada9bb3d1993405
Анастасия Москвина <A.E.Moskvina@yandex.ru>
2024-06-05 00:00:12 +0300

    models Genre and Title v1

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -11,12 +11,30 @@ class Category(models.Model):
 
 
 class Genre(models.Model):
-    pass
+    """Модель для представления жанра произведения."""
+
+    name = models.CharField(max_length=256, vernose_name='Название')
+    slug = models.SlugField(max_length=50, verbose_name='Slug', unique=True)
+
+    class Meta:
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
 
 
 class Title(models.Model):
-    pass
-    blablabla
+    """Модель для хранения информации о произведении."""
+
+    name = models.CharField(max_length=256, verbose_name='Название')
+    year = models.IntegerField(verbose_name='Год выпуска')
+    description = models.TextField(null=True, verbose_name='Описание')
+    genre = models.ManyToManyField('Genre', verbose_name='Жанр')
+    category = models.ForeignKey(
+        'Category', on_delete=models.CASCADE, verbose_name='Категория')
+
+    class Meta:
+        default_related_name = 'titles'
+        verbose_name = 'Произведение'
+        verbose_name_plural = 'Произведения'
 
 
 class Review(models.Model):
