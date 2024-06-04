8d8bdd97d498623b489a907f64077b087a82894a
Джейран Сущева <dzheiran.s@yandex.ru>
2024-06-04 23:42:01 +0300

    models Review and Comment v.1

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 
@@ -16,17 +17,38 @@ class Genre(models.Model):
 
 class Title(models.Model):
     pass
-    blablabla
 
 
 class Review(models.Model):
-    pass
+    """Модель отзыва к произведению."""
+
+    title__id = models.IntegerField()  # Временная заглушка пока нет произведения.
+    text = models.TextField(verbose_name='Текст отзыва')
+    author = models.IntegerField(verbose_name='Автор отзыва')  # Временная заглушка пока нет юзера.
+    score = models.IntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(10)],
+        verbose_name='Рейтинг'
+    )
+    pub_date = models.DateTimeField(
+        auto_now_add=True, verbose_name='Дата и время отзыва')
+
+    class Meta:
+        default_related_name = 'reviews'
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
 
 
 class Comment(models.Model):
-    """Модель комментария к произведению."""
+    """Модель комментария к отзыву."""
 
+    review_id = models.ForeignKey(
+        Review,
+        on_delete=models.CASCADE,
+    )
     text = models.TextField(verbose_name='Текст комментария')
+    author = models.IntegerField(verbose_name='Автор комментария')  # Временная заглушка пока нет юзера.
+    pub_date = models.DateTimeField(
+        auto_now_add=True, verbose_name='Дата и время комментария')
 
     class Meta:
         default_related_name = 'comments'
