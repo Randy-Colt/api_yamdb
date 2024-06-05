ea61f4170dc3d5f532b4b5204c50e2885b1d0945
Анастасия Москвина <A.E.Moskvina@yandex.ru>
2024-06-05 23:10:41 +0300

    urls for title genre

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,5 +1,11 @@
 from django.urls import include, path
-from rest_framework import routers
+from rest_framework.routers import DefaultRouter
+from .views import TitlesViewSet
+
+router_v1 = DefaultRouter()
+
+router_v1.register('titles', TitlesViewSet, basename='titles')
+router_v1.register('genres', GenreViewSet, basename='genres')
 
 
 
