887bbcd629aae8bab12a4b220851739e62f9b0bd
Андрей Логвинов <andryu04101999@gmail.com>
2024-06-05 17:19:04 +0300

    user and category models

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -3,11 +3,51 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    """Модель пользователя."""
+
+    class Role(models.TextChoices):
+        USER = 'user'
+        MODER = 'moderator'
+        ADMIN = 'admin'
+
+    email = models.EmailField('Почта', unique=True)
+    bio = models.TextField(
+        verbose_name='Биография',
+        max_length=300,
+        blank=True,
+        null=True
+    )
+    role = models.CharField(
+        verbose_name='Роль',
+        max_length=10,
+        choices=Role.choices,
+        default=Role.USER
+    )
+    confirmation_code = models.CharField(
+        unique=True,
+        blank=True,
+        null=True,
+        editable=False
+    )
 
 
 class Category(models.Model):
-    pass
+    """Модель категории."""
+
+    name = models.CharField(
+        verbose_name='Категория',
+        max_length=256,
+        unique=True
+    )
+    slug = models.SlugField(
+        verbose_name='Слаг',
+        max_length=50,
+        unique=True
+    )
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'Категории'
 
 
 class Genre(models.Model):
