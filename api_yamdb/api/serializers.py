90e9ee4a2c3b1e1c0d9deb1df66cc88c4a5449c9
Джейран Сущева <dzheiran.s@yandex.ru>
2024-06-07 00:11:00 +0300

    Review_Comment serial & view

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,6 +1,13 @@
 from rest_framework import serializers
-from reviews.models import Title, Genre, Category
-                            
+
+from reviews.models import (
+    Category,
+    Comment,
+    Genre,
+    Review,
+    Title,
+)
+
 
 class TitleSerializer(serializers.ModelSerializer):
     """Сериализатор для модели Title."""
@@ -13,13 +20,13 @@ class TitleSerializer(serializers.ModelSerializer):
         queryset=Category.objects.all(),
         slug_field='slug'
     )
-    # rating = serializers.IntegerField(read_only=True)
+    rating = serializers.IntegerField(read_only=True)
 
     class Meta:
         model = Title
-        fields = ('id', 'name', 'year', 'description',
+        fields = ('id', 'name', 'year', 'rating', 'description',
                   'genre', 'category')
-    
+
 
 class GenreSerializer(serializers.ModelSerializer):
     """Сериализатор для модели Genre."""
@@ -28,3 +35,28 @@ class GenreSerializer(serializers.ModelSerializer):
         model = Genre
         fields = ('name', 'slug')
 
+
+class ReviewSerializer(serializers.ModelSerializer):
+    """Сериализатор для модели Отзывов."""
+    # Закомментила пока нет авторизации!
+    # author = serializers.SlugRelatedField(
+    #     read_only=True,
+    #     slug_field='username'
+    # )
+
+    class Meta:
+        fields = ('id', 'text', 'author', 'score', 'pub_date')
+        model = Review
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    """Сериализатор для модели Комментариев."""
+    # Закомментила пока нет авторизации!
+    # author = serializers.SlugRelatedField(
+    #     read_only=True,
+    #     slug_field='username'
+    # )
+
+    class Meta:
+        fields = ('id', 'text', 'author', 'pub_date')
+        model = Comment
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,15 +1,25 @@
 from django.db.models import Avg
+from django.shortcuts import get_object_or_404
 from rest_framework.permissions import IsAdminUser
 from rest_framework import viewsets
 
-from reviews.models import Title, Genre
-                           
-from .serializers import GenreSerializer, TitleSerializer
-                          
+from .serializers import (
+    CommentSerializer,
+    GenreSerializer,
+    ReviewSerializer,
+    TitleSerializer
+)
+from reviews.models import (
+    Genre,
+    Review,
+    Title
+)
 
 
 class TitleViewSet(viewsets.ModelViewSet):
-    queryset = Title.objects.all()
+    queryset = Title.objects.all().annotate(
+        rating=Avg('reviews__score')
+    )
     serializer_class = TitleSerializer
 
 
@@ -19,8 +29,50 @@ class GenreViewSet(viewsets.ModelViewSet):
 
 
 class ReviewViewSet(viewsets.ModelViewSet):
-    pass
+    """Вьюсет для получения списка отзывов, либо одельного отзыва."""
+
+    serializer_class = ReviewSerializer
+    http_method_names = ['get', 'post', 'delete', 'patch']
+
+    def get_title(self):
+        title_id = self.kwargs.get('title_id')
+        title = get_object_or_404(Title, id=title_id)
+        return title
+
+    def get_queryset(self):
+        title = self.get_title()
+        queryset = title.reviews.all()
+        return queryset
+
+    def perform_create(self, serializer):
+        title = self.get_title()
+        serializer.save(
+            # Закомментила пока нет авторизации!
+            # author=self.request.user,
+            title_id=title
+        )
 
 
 class CommentViewSet(viewsets.ModelViewSet):
-    pass
+    """Вьюсет для получения списка комментариев, либо одельного комментария."""
+
+    serializer_class = CommentSerializer
+    http_method_names = ['get', 'post', 'delete', 'patch']
+
+    def get_review(self):
+        review_id = self.kwargs.get('review_id')
+        review = get_object_or_404(Review, id=review_id)
+        return review
+
+    def get_queryset(self):
+        review = self.get_review()
+        queryset = review.comments.all()
+        return queryset
+
+    def perform_create(self, serializer):
+        review = self.get_review()
+        serializer.save(
+            # Закомментила пока нет авторизации!
+            # author=self.request.user,
+            review_id=review
+        )
