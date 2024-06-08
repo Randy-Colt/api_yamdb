6f4bb1e5c6d55f529f482dc3632c5357e99885d5
Андрей Логвинов <andryu04101999@gmail.com>
2024-06-08 14:37:44 +0300

    without tokens

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,17 +1,5 @@
 from rest_framework import permissions
 
-# class IsAuthorOrReadOnly(permissions.BasePermission):
-#     message = 'У вас нет доступа для совершения этого действия.'
-
-#     def has_permission(self, request, view):
-#         return (
-#                 request.method in permissions.SAFE_METHODS
-#                 or request.user.is_authenticated
-#             )
-
-#     def has_object_permission(self, request, view, obj):
-#         return obj.user == request.user
-
 
 class IsAuthorAdminModerator(permissions.BasePermission):
 
--- api_yamdb/users/urls.py
+++ api_yamdb/users/urls.py
@@ -1,14 +1,9 @@
 from django.urls import path
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 from api.constants import API_VERSION
 
 urlpatterns = [
     path(f'{API_VERSION}/auth/token/',
          TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path(f'{API_VERSION}/auth/token/refresh/',
-         TokenRefreshView.as_view(), name='token_refresh'),
 ]
