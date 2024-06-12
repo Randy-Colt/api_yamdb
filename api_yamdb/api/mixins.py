70595767c00c77a50d47a6a5af176f9946375d86
Андрей Логвинов <andryu04101999@gmail.com>
2024-06-12 22:53:25 +0300

    fix mixin

--- api_yamdb/api/mixins.py
+++ api_yamdb/api/mixins.py
@@ -1,6 +1,8 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, mixins, viewsets
+
 from .permissions import IsAdminOrReadOnly
+
 
 class ListCreateDeleteMixin(mixins.ListModelMixin,
                             mixins.CreateModelMixin,
